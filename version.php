bf3582ef63881cba0d33a934d82d0f69b7c97f8f
Bruno Magalhães <brunomagalhaes@blackbean.com.br>
2018-05-23 11:13:03 -0300

    Moodle Plugin Approval Process - Take 1
    - Replacing the "<=>" operator with the generic "=" SQL92 native operator;
    - Converting all tabs into spaces;
    - Replacing your "cleaned" GNU license statement with the Moody's default one.

--- version.php
+++ version.php
@@ -1,24 +1,20 @@
 <?php
-/**
- * This file is part of BlackBean LiveUsers <https://github.com/blackbean/BBLiveUsers>, a plugin
- * for the Moodle LMS Platform <https://www.moodle.org> that monitors the exact number of online
- * users per course in realtime, even if they spend hours watching a single video, for example,
- * without ever refreshing a single page. Copyright (C) 2018 by BlackBean Technologies Ltda.
- *
- * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation, either version
- * 3 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU
- * General Public License along with this program.
- * If not, see https://www.gnu.org/licenses/
- */
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
  * @package block_bbliveusers
  * @author Bruno Magalhães <brunomagalhaes@blackbean.com.br>
@@ -36,5 +32,5 @@ $plugin->version = 2018041200;
 $plugin->maturity = MATURITY_BETA;
 $plugin->requires = 2016052311;
 $plugin->dependencies = [
-	'local_bbliveusers' => 2018041200
+    'local_bbliveusers' => 2018041200
 ];
