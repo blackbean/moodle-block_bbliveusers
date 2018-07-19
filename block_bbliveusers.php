315701b5cf7723b47b223de7c6e1b22ca56e4d7b
Bruno Magalhães <brunomagalhaes@blackbean.com.br>
2018-07-19 17:36:53 -0300

    Refactoring to make MoodlePluginCI happy.

--- block_bbliveusers.php
+++ block_bbliveusers.php
@@ -44,18 +44,15 @@ class block_bbliveusers extends block_base
         $courseid = isset($COURSE->id) ? max(0, (integer)$COURSE->id) : 0;
         $limit = 20;
 
-        if ($this->content !== null) {
-            return($this->content);
-        }
-
-        $this->content = new stdClass;
-        $this->content->header = '';
-        $this->content->text = '<div id="block-bbliveusers-chart"><noscript>'.
-                                get_string('message_javascript', 'block_bbliveusers').
-                                '</noscript></div>';
-        $this->content->footer = '<link type="text/css" href="/blocks/bbliveusers/chartist.css" media="all" rel="stylesheet"/>';
-        $this->content->footer .= '<script type="text/javascript" src="/blocks/bbliveusers/chartist.js"></script>';
-        $this->content->footer .= '<script type="text/javascript">
+        if (empty($this->content)) {
+            $this->content = new stdClass;
+            $this->content->header = '';
+            $this->content->text = '<div id="block-bbliveusers-chart"><noscript>'.
+                                    get_string('message_javascript', 'block_bbliveusers').
+                                    '</noscript></div>';
+            $this->content->footer = '<link type="text/css" href="/blocks/bbliveusers/chartist.css" media="all"/>';
+            $this->content->footer .= '<script type="text/javascript" src="/blocks/bbliveusers/chartist.js"></script>';
+            $this->content->footer .= '<script type="text/javascript">
 var toggle = 0;
 var labels = ["","","","","","","","","","","","","","","","","","","","","","","","","",'.
              '"","","","","","","","","","","","","","","","","","","","","","","","",""];
@@ -64,23 +61,9 @@ var values = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'.
 var chart = new Chartist.Bar("#block-bbliveusers-chart",{series:[values]},{
 fullWidth: true,
 seriesBarDistance: 0,
-chartPadding: {
-    top: 20,
-    left: 0,
-    right: 0,
-    bottom: 0},
-axisX: {
-    onlyInteger: true,
-    showLabel: true,
-    showGrid: false,
-    offset: 15
-},
-axisY: {
-    onlyInteger: true,
-    showLabel: true,
-    showGrid: true,
-    offset: 30
-}});
+chartPadding: {top: 20, left: 0, right: 0, bottom: 0},
+axisX: {onlyInteger: true, showLabel: true, showGrid: false, offset: 15},
+axisY: {onlyInteger: true, showLabel: true, showGrid: true, offset: 30}});
 window.setInterval(function(){
     if(window.XMLHttpRequest){
         request = new XMLHttpRequest();
@@ -120,24 +103,18 @@ window.setInterval(function(){
                     values.shift();
                     if(block = document.getElementById("block-bbliveusers-chart")){
                         if(block.clientWidth<200){
-                            chart.update({labels:labels.slice(-10),
-                                          series:[values.slice(-10)]});
+                            chart.update({labels:labels.slice(-10), series:[values.slice(-10)]});
                         } else if(block.clientWidth<400){
-                            chart.update({labels:labels.slice(-20),
-                                          series:[values.slice(-20)]});
+                            chart.update({labels:labels.slice(-20), series:[values.slice(-20)]});
                         } else if(block.clientWidth<600){
-                            chart.update({labels:labels.slice(-30),
-                                          series:[values.slice(-30)]});
+                            chart.update({labels:labels.slice(-30), series:[values.slice(-30)]});
                         } else if(block.clientWidth<800){
-                            chart.update({labels:labels.slice(-40),
-                                          series:[values.slice(-40)]});
+                            chart.update({labels:labels.slice(-40), series:[values.slice(-40)]});
                         } else {
-                            chart.update({labels:labels,
-                                          series:[values]});
+                            chart.update({labels:labels, series:[values]});
                         }
                     } else {
-                        chart.update({labels:labels,
-                                      series:[values]});
+                        chart.update({labels:labels, series:[values]});
                     }
                     toggle += 1;
                 }
@@ -148,6 +125,7 @@ window.setInterval(function(){
     request.send();
 }, '.$limit.'*1000);
 </script>';
+        }
 
         return($this->content);
     }
